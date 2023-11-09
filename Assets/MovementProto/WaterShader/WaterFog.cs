ab63a82f1d0108bb072037d9fa7c26c9d2c6bb82
Jared Goronkin <jgoronkin@gmail.com>
2023-11-08 20:58:49 -0500

    Fixed Merge Bug

--- Assets/MovementProto/WaterShader/WaterFog.cs
+++ Assets/MovementProto/WaterShader/WaterFog.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnderWaterEffect : MonoBehaviour
+public class WaterFog : MonoBehaviour
 {
     [SerializeField] private float WaterLevel;
     private bool IsUnderwater;
