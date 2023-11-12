7d981b9a5e5f32ae54930814ff5459c176407cb7
Tessla Muir <tesslamuir@yahoo.com>
2023-11-12 01:00:41 -0500

    Button Listeners

--- Assets/UI/LevelSelectUI.cs
+++ Assets/UI/LevelSelectUI.cs
@@ -10,18 +10,20 @@ public class LevelSelectUI : MonoBehaviour
     [SerializeField] Transform levelRoot;
     [SerializeField] GameObject unlockedPrefab;
     [SerializeField] GameObject lockedPrefab;
-
-    void Awake()
-    {
-        // Keep across scenes
-        DontDestroyOnLoad(this.gameObject);
-    }
+    List<int> nums = new List<int>();
 
     void Start()
     {
         // Find game manager and setup event listener
         gameManager = GameObject.FindObjectOfType<GameManager>();
         gameManager.onStateUpdated += UpdateUI;
+
+        // Fill out list
+        for (int i = 0; i < gameManager.GetLevelCount(); i++)
+        {
+            nums.Add(i);
+        }
+
         UpdateUI();
     }
 
@@ -33,23 +35,26 @@ public class LevelSelectUI : MonoBehaviour
             Destroy(child.gameObject);
         }
 
-        // Add the levels
-        for (int i = 0; i < gameManager.GetLevelCount(); i++)
+        // Add the levels -- must use the list compared to iterating
+        foreach (int num in nums)
         {
+            Debug.Log(num);
             // Create the level instance
-            GameObject levelInstance = Instantiate(gameManager.IsLevelUnlocked(i) ? unlockedPrefab : lockedPrefab, levelRoot);
+            GameObject levelInstance = Instantiate(gameManager.IsLevelUnlocked(num) ? unlockedPrefab : lockedPrefab, levelRoot);
 
             // Set text for level & fish count
-            levelInstance.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Level " + i;
-            levelInstance.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>().text = gameManager.GetCollectedFish(i) + "/" + gameManager.GetTotalFish(i);
+            levelInstance.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Level " + num;
+            levelInstance.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>().text = gameManager.GetCollectedFish(num) + "/" + gameManager.GetTotalFish(num);
 
             // Add button
-            if (gameManager.IsLevelUnlocked(i))
+            if (gameManager.IsLevelUnlocked(num))
             {
+                Debug.Log("Set button to " + num);
                 Button button = levelInstance.GetComponent<Button>();
                 button.onClick.AddListener(() =>
                 {
-                    gameManager.SetCurrentLevel(i, 0); 
+                    gameManager.SetCurrentLevel(num, 10); 
+                    gameObject.SetActive(false);
                 });
             }
         }
